ddcafb078c7c310325f799d15d2992876a1336a9
Anusha <anushavysakh232@gmail.com>
2022-07-04 13:11:54 +0530

    Random Single digit

--- rand1.sh
+++ rand1.sh
@@ -7,5 +7,13 @@ do
 done
 
 
+#2) Use Random to get Dice Number between 1 to 6
 
+echo $(( ($RANDOM % 6 ) +1 ))
 
+#3)Add two Random Dice Number and Print the Result
+
+a=$(( ($RANDOM % 6 ) +1 ))
+b=$(( ($RANDOM % 6 ) +1 ))
+sum=$((a+b))
+echo $sum
